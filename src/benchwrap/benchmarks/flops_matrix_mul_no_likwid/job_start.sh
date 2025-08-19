e748636ec1b6ed5f2703b54721a61bacb645d824
Davide <davide.mattioli.2001@gmail.com>
2025-08-19 11:11:53 +0200

    fix

--- src/benchwrap/benchmarks/flops_matrix_mul_no_likwid/job_start.sh
+++ src/benchwrap/benchmarks/flops_matrix_mul_no_likwid/job_start.sh
@@ -8,16 +8,18 @@
 #SBATCH --acctg-freq=energy=1
 
 set -euo pipefail
-export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 source activate test
+
+
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+
 #Create job dir
 DEST="$HOME/.local/share/benchwrap/job_${SLURM_JOB_ID}"
 mkdir -p "$DEST"
 
-srun python3 -u -m benchwrap.benchmarks.flops_matrix_mul.workload 1>&2
+srun python3 benchwrap.benchmarks.flops_matrix_mul.workload 1>&2
 
 
-echo "Results -> $DEST"
 
 
 
