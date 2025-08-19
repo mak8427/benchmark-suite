20596c6044e28cb0c314b30002e87a2b30ba644e
Davide <davide.mattioli.2001@gmail.com>
2025-08-19 17:28:06 +0200

    fix: update ior_test job script to request 2 nodes and allocate resources

--- src/benchwrap/benchmarks/ior_test/job_start.sh
+++ src/benchwrap/benchmarks/ior_test/job_start.sh
@@ -18,6 +18,7 @@ cd "$DEST"
 
 CONF_DIR="$DEST/ior_inis"
 for cfg in "$CONF_DIR"/*.ini; do
+  echo"$(findmnt -T . -o TARGET,FSTYPE,SOURCE)"
   mpirun -np 4 /home/davide.mattioli/my_beegfs/_BEEGFS/davide.mattioli/io500-small-exercise/io500/bin/ior -f "$cfg"
 done
 
