af51aa4dc05481f27ba55007921b0d107c35db93
Davide <davide.mattioli.2001@gmail.com>
2025-08-19 16:02:55 +0200

    test: add ior_test executor module with argparse support

---
+++ src/benchwrap/executors/ior_test.py
@@ -0,0 +1,15 @@
+import argparse
+
+from .utils.benchmarks_func import *
+
+
+def main():
+    p = argparse.ArgumentParser()
+    p.add_argument("--partition", required=False)
+
+    args = p.parse_args()
+    run_slurm_job(bench_name="ior_test", partition=args.partition)
+
+
+if __name__ == "__main__":
+    main()
