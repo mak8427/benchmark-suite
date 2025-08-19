7defaa6463d32ae82c45b202462df56f4ade0cd3
Davide <davide.mattioli.2001@gmail.com>
2025-08-19 16:05:41 +0200

    fix: handle missing partition argument in ior_test executor

--- src/benchwrap/executors/ior_test.py
+++ src/benchwrap/executors/ior_test.py
@@ -4,12 +4,17 @@ from .utils.benchmarks_func import *
 
 
 def main():
-    p = argparse.ArgumentParser()
-    p.add_argument("--partition", required=False)
 
-    args = p.parse_args()
-    run_slurm_job(bench_name="ior_test", partition=args.partition)
+    try:
+        p = argparse.ArgumentParser()
+        p.add_argument("--partition", required=False)
 
+        args = p.parse_args()
+        run_slurm_job(bench_name="ior_test", partition=args.partition)
+
+    except:
+        print("No partition specified")
+        run_slurm_job(bench_name="ior_test", partition=None)
 
 if __name__ == "__main__":
     main()
