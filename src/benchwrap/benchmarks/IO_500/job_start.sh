8f7ada316574ee933a3d0130039cea483b5a22f5
Davide <davide.mattioli.2001@gmail.com>
2025-08-20 16:59:29 +0200

    fix: update IO_500 job script to adjust node and process count for scalability

--- src/benchwrap/benchmarks/IO_500/job_start.sh
+++ src/benchwrap/benchmarks/IO_500/job_start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=swarm_manager
-#SBATCH --nodes=2
+#SBATCH --job-name=IO_5ẞẞ
+#SBATCH --nodes=4
 #SBATCH --profile=all
 #SBATCH --acctg-freq=1
 #SBATCH --acctg-freq=energy=1
 
 set -euo pipefail
-salloc -N2 --exclusive
+salloc -N4
 module load mpi
 
 DEST="$HOME/.local/share/benchwrap/job_${SLURM_JOB_ID}"
@@ -15,7 +15,7 @@ mkdir -p "$DEST"
 
 IO500="$HOME/.local/share/benchwrap/benchmarks/io500"
 
-mpirun -np 2 "$IO500/io500.sh" -f "$IO500/config-minimal.ini"
+mpirun -np 4 "$IO500/io500.sh" "$IO500/config-minimal.ini"
 
 echo "$(ls)"
 
