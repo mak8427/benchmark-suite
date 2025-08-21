66f06b4545aab650d2762838190fa3108c91e21b
Davide <davide.mattioli.2001@gmail.com>
2025-08-21 16:32:38 +0200

    fix: remove unused SLURM options in job_start.sh

--- src/benchwrap/benchmarks/IO_500/job_start.sh
+++ src/benchwrap/benchmarks/IO_500/job_start.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 #SBATCH -n 1
 #SBATCH --job-name=IO_500
-#SBATCH --profile=all
-#SBATCH --acctg-freq=1
-#SBATCH --acctg-freq=energy=1s
 
 
-module load mpi
 
 
 DEST="$HOME/.local/share/benchwrap/jobs/job_${SLURM_JOB_ID}"
 IO500="$HOME/.local/share/benchwrap/benchmarks/io500"
 cd "$DEST"
 
-srun "$IO500/io500.sh" "$IO500/config-minimal.ini"
+srun  "$IO500/io500.sh" "$IO500/config-minimal.ini"
 
 echo "$(ls)"
 
