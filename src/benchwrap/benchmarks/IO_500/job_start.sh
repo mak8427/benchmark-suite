b906742a991faa866995e47289b8f32d168f9d01
Davide <davide.mattioli.2001@gmail.com>
2025-08-21 16:10:38 +0200

    fix: add DEST variable initialization in job_start.sh

--- src/benchwrap/benchmarks/IO_500/job_start.sh
+++ src/benchwrap/benchmarks/IO_500/job_start.sh
@@ -4,20 +4,16 @@
 #SBATCH --acctg-freq=1
 #SBATCH --acctg-freq=energy=1s
 
-set -euo pipefail
+
 module purge
-module load openmpi
-
-
-
-
+module load mpi
 
 
 DEST="$HOME/.local/share/benchwrap/jobs/job_${SLURM_JOB_ID}"
 IO500="$HOME/.local/share/benchwrap/benchmarks/io500"
 cd "$DEST"
 
-mpiexec "$IO500/io500.sh" "$IO500/config-scc.ini"
+mpiexec "$IO500/io500.sh" "$IO500/config-minimal.ini"
 
 echo "$(ls)"
 
