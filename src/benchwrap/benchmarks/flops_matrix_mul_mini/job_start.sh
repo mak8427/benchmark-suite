d659dffe15d7abc01e277a455b2abc8d2119c13d
Davide <davide.mattioli.2001@gmail.com>
2025-10-21 11:51:17 +0200

    make excls

--- src/benchwrap/benchmarks/flops_matrix_mul_mini/job_start.sh
+++ src/benchwrap/benchmarks/flops_matrix_mul_mini/job_start.sh
@@ -2,6 +2,8 @@
 #SBATCH --profile=all
 #SBATCH --acctg-freq=1
 #SBATCH --acctg-freq=energy=1
+#SBATCH --exclusive
+
 
 set -euo pipefail
 module load likwid
@@ -14,6 +16,4 @@ srun --cpu-bind=cores \
   likwid-perfctr -g FLOPS_DP -t 1s \
   python3 -u -m benchwrap.benchmarks.flops_matrix_mul_mini.workload 1>&2 \
 
-benchwrap run flops_matrix_mul scc-cpu
-
-echo "Results -> $DEST"
+benchwrap run flops_matrix_mul_mini -p  scc-cpu
